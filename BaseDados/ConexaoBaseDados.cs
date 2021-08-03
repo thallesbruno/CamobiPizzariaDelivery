f89b5f58525b591d35a8ddde6ca92aa34601579b
Thalles Bruno <thallesbgns@gmail.com>
2021-08-03 20:18:22 -0300

    Conexão

---
+++ BaseDados/ConexaoBaseDados.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDados
+{
+    public class ConexaoBaseDados
+    {
+        private static readonly ConexaoBaseDados instanciaMySQL = new ConexaoBaseDados();
+
+        public static ConexaoBaseDados getInstancia()
+        {
+            return instanciaMySQL;
+        }
+
+        public MySqlConnection getConexao()
+        {
+            string conn = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+            return new MySqlConnection(conn);
+        }
+    }
+}
