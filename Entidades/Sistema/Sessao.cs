4ee16f8c0a5141407b7a64d60a23e9c40d657bb8
Thalles Bruno <thallesbgns@gmail.com>
2021-08-15 21:14:26 -0300

    Sessoes

---
+++ Entidades/Sistema/Sessao.cs
@@ -0,0 +1,22 @@
+﻿using Entidades.Pessoas;
+
+namespace Entidades.Sistema
+{
+    public class Sessao
+    {
+        public static Entidade Usuario { get; set; }
+        public static TipoUsuario TipoUsuario { get; set; }
+        private static Sessao Instance;
+        public static Sessao GetInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    Instance = new Sessao();
+                }
+                return Instance;
+            }
+        }
+    }
+}
