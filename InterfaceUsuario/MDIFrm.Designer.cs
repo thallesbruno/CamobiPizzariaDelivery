7b0c018952c718cd57961f3147b38a0dbd18c910
Thalles Bruno <thallesbgns@gmail.com>
2021-08-18 22:41:09 -0300

    MenuStrip

--- InterfaceUsuario/MDIFrm.Designer.cs
+++ InterfaceUsuario/MDIFrm.Designer.cs
@@ -30,7 +30,60 @@ namespace InterfaceUsuario
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MDIFrm));
+            this.mnsPrincipal = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rotinasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatõriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsPrincipal.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // mnsPrincipal
+            // 
+            this.mnsPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.cadastrosToolStripMenuItem,
+            this.rotinasToolStripMenuItem,
+            this.relatõriosToolStripMenuItem});
+            this.mnsPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.mnsPrincipal.Name = "mnsPrincipal";
+            this.mnsPrincipal.Size = new System.Drawing.Size(1393, 24);
+            this.mnsPrincipal.TabIndex = 1;
+            this.mnsPrincipal.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            this.cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // rotinasToolStripMenuItem
+            // 
+            this.rotinasToolStripMenuItem.Name = "rotinasToolStripMenuItem";
+            this.rotinasToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.rotinasToolStripMenuItem.Text = "Rotinas";
+            // 
+            // relatõriosToolStripMenuItem
+            // 
+            this.relatõriosToolStripMenuItem.Name = "relatõriosToolStripMenuItem";
+            this.relatõriosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.relatõriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
             // 
             // MDIFrm
             // 
@@ -38,17 +91,29 @@ namespace InterfaceUsuario
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::InterfaceUsuario.Properties.Resources.fundo;
             this.ClientSize = new System.Drawing.Size(1393, 693);
+            this.Controls.Add(this.mnsPrincipal);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnsPrincipal;
             this.Name = "MDIFrm";
             this.Text = "Camobi Pizzaria Delivery";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.MDIFrm_Load);
+            this.mnsPrincipal.ResumeLayout(false);
+            this.mnsPrincipal.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mnsPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rotinasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatõriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
     }
 }
 
--- InterfaceUsuario/MDIFrm.cs
+++ InterfaceUsuario/MDIFrm.cs
@@ -22,9 +22,19 @@ namespace InterfaceUsuario
                     break;
                 }
             }
+
+            mnsPrincipal.Visible = false;
+
             var frmLogin = new FrmLogin();
             frmLogin.ShowDialog();
             if (!frmLogin.bFlagLogin) Application.Exit();
+
+            mnsPrincipal.Visible = true;
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
