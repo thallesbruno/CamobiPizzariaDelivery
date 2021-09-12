b060e1e4b366c7bc0dc1f608a5fb91bc02740b44
Thalles Bruno <thallesbgns@gmail.com>
2021-09-11 22:35:40 -0300

    Validacoes

---
+++ InterfaceUsuario/Modulos/MascaraCampoCodigo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfaceUsuario.Modulos
+{
+    public static class MascaraCampoCodigo
+    {
+        public static void RetornarMascara(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (txt.Text.Trim().Equals(string.Empty))
+                return;
+            txt.Text = Int32.Parse(txt.Text).ToString("00000");
+        }
+
+        public static void TirarMascara(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (txt.Text.Trim().Equals(string.Empty))
+                return;
+            txt.Text = Convert.ToInt32(txt.Text.Trim()).ToString();
+            txt.Focus();
+            txt.Select(0, txt.Text.Length);
+        }
+
+        public static void ApenasValoresNumericos(object sender, KeyPressEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(Keys.Back))
+                e.Handled = true;
+        }
+
+        public static void AplicarEventos(TextBox txt)
+        {
+            txt.Enter += TirarMascara;
+            txt.Leave += RetornarMascara;
+            txt.KeyPress += ApenasValoresNumericos;
+        }
+    }
+}
